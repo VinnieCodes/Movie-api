ad479c72de1b619a3f918cf1fc234bdc48278c62
Vinnie <vincentathorne2005@gmail.com>
2024-04-03 11:32:14 -0700

    adding movies

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-const http = require("http"),
-  fs = require("fs"),
-  url = require("url");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const http = require("http");
+
+app.use(express.static("public"));
+
+app.use(morgan("dev"));
+
+let topMovies = [
+  { title: "Shrek", year: "2001" },
+  { title: "Shrek 2", year: "2004" },
+  { title: "Shrek the Third", year: "2007" },
+  { title: "Shrek the Halls", year: "2007" },
+  { title: "Shrek Forever After", year: "2010" },
+  { title: "Shrek 5", year: "2025" },
+  { title: "The Shawshank Redemption", year: "1994" },
+  { title: "The Godfather", year: "1972" },
+  { title: "The Godfather: Part II", year: "1974" },
+  { title: "The Dark Knight", year: "2008" },
+];
+
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to my movie API. Add /movies to the URL to get a list of top 10 movies"
+  );
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 http
   .createServer((request, response) => {
-    let addr = request.url,
-      q = new URL(addr, "http://" + request.headers.host),
-      filePath = "";
-
-    fs.appendFile(
-      "log.txt",
-      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
-      (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Added to log.");
-        }
-      }
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end(
+      "Welcome to my movie API. Add /movies to the URL to get a list of top 10 movies"
     );
-
-    if (q.pathname.includes("documentation")) {
-      filePath = __dirname + "/documentation.html";
-    } else {
-      filePath = "index.html";
-    }
-
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        throw err;
-      }
-
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(data);
-      response.end();
-    });
   })
   .listen(8080);
-console.log("My test server is running on Port 8080.");
+
+console.log("Server running on Port 8080.");
