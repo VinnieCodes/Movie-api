30fa108935f56e21fa35a57940c98a3ee0cd9a96
Vinnie <vincentathorne2005@gmail.com>
2024-04-26 11:36:22 -0700

    heroku

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
+// mongoose.connect("mongodb://localhost:27017/dbname", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect(process.env.CONNECTION_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 let users = [
   {
     id: 1,
